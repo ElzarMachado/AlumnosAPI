c21802a3d91c64b033f8246b2b34d4729c526981
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-11-12 20:12:33 -0700

    Add project files.

---
+++ AlumnosAPI/Model/Entities/Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlumnosAPI.Model.Entities
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+
+        public List<Materia> Materias { get; set; }
+    }
+}
---
+++ AlumnosAPI/Model/Entities/Materia.cs
@@ -0,0 +1,9 @@
+﻿namespace AlumnosAPI.Model.Entities
+{
+    public class Materia
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
